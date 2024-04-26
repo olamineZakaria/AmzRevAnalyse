e391067df4a4864707f7148e0ead6b77007ec938
olamine zakaria <tentzakaria@gmail.com>
2024-04-26 22:08:18 +0100

    first commit

---
+++ Amazon.py
@@ -0,0 +1,106 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+import re
+import csv
+import pandas as pd
+import re
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+import spacy
+from textblob import TextBlob
+import streamlit as st
+import plotly.express as px
+import requests
+import subprocess
+from wordcloud import WordCloud
+
+nlp = spacy.load("en_core_web_sm")
+#subprocess.run(["python", "-m", "spacy", "download", "en_core_web_sm"])
+stop_words = set(stopwords.words('english'))
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_driver_path = "chromedriver.exe" 
+service = Service(chrome_driver_path)
+driver = webdriver.Chrome(service=service, options=chrome_options)
+def get_url_review_page(UrlProduct,i):
+    UrlReview = UrlProduct.replace('dp','product-reviews') + '?pageNumber=' + str(i)
+    return UrlReview
+def get_rating(UrlProduct):
+    driver.get(get_url_review_page(UrlProduct,1))
+    soup = BeautifulSoup(driver.page_source,'html.parser')
+    rating = soup.select("#cm_cr-product_info > div > div.a-text-left.a-fixed-left-grid-col.reviewNumericalSummary.celwidget.a-col-left > div.a-row.a-spacing-small.averageStarRatingIconAndCount > div > div > div.a-fixed-left-grid-col.aok-align-center.a-col-right > div > span")
+    rating = str(rating)
+    g=rating[73:-17]
+    return g
+def get_product_name(UrlProduct):
+    driver.get(get_url_review_page(UrlProduct,1))
+    soup = BeautifulSoup(driver.page_source,'html.parser')
+    Product_name = soup.find('a',{'data-hook':"product-link"})
+    # Product_name = str(Product_name.string)
+    Product_name = str(Product_name)
+    pattern = r'>([^<]+)<'
+    Product_name = re.findall(pattern, Product_name)
+    return Product_name[0]
+def get_global_rating(UrlProduct):
+    driver.get(get_url_review_page(UrlProduct,1))
+    soup = BeautifulSoup(driver.page_source,'html.parser')
+    Global_rating = soup.find('span',{'class':'a-color-secondary'})
+    Global_rating = str(Global_rating)
+    g = Global_rating[44:-7]
+    return g
+def get_image_url(UrlProduct):
+    driver.get(UrlProduct)
+    soup = BeautifulSoup(driver.page_source,'html.parser')
+    Image_of_product = soup.select('#landingImage')
+    Image_of_product = re.findall(r'src="https.*pg',str(Image_of_product[0]))
+    Image_of_product =  str(Image_of_product)
+    Image_of_product = Image_of_product[7:-2]
+    return Image_of_product
+def extract_reviews(UrlProduct):
+    driver.get(get_url_review_page(UrlProduct,1))
+    soup = BeautifulSoup(driver.page_source,'html.parser')
+    review_spans = soup.find_all('span', class_='review-text-content')
+    reviews = [review.get_text() for review in review_spans]
+    return reviews
+def extract_all_reviews(UrlProduct):
+    all_reviews = []
+    url_review = get_url_review_page(UrlProduct,1)
+    numberOfpages = 10
+    for i in range(1,numberOfpages+1):
+        url_review = get_url_review_page(UrlProduct,1)
+        reviews = extract_reviews(url_review)
+        all_reviews.extend(reviews)
+        # with open("file.txt", "a", encoding="utf-8") as file:
+        #     for review in reviews:
+        #         file.write(review + "\n")
+        # print("Page", i)
+    return all_reviews
+def clean_comment(comment):
+    comment = comment.lower()
+    comment = re.sub(r'[^a-zA-Z\s]', '', comment)
+    comment = re.sub(r'\d+', '', comment)
+    tokens = word_tokenize(comment)
+    tokens = [token for token in tokens if token not in stop_words]
+    cleaned_comment = ' '.join(tokens)
+    return cleaned_comment
+def sentiment_analysis_textblob(text):
+    doc = nlp(text)
+    blob = TextBlob(doc.text)
+    polarity = blob.sentiment.polarity
+    if polarity > 0:
+        sentiment_label = "Positive"
+    elif polarity < 0:
+        sentiment_label = "Negative"
+    else:
+        sentiment_label = "Neutral"
+    return sentiment_label
+def comment_dataFrame(UrlProduct):
+    reviews = extract_all_reviews(UrlProduct)
+    reviews_cleaned = [clean_comment(review.replace("\n", " ")) for review in reviews]
+    df = pd.DataFrame(reviews_cleaned, columns=['comment'])
+    df['sentiment'] = df['comment'].apply(sentiment_analysis_textblob)
+    return df
+def sentiemnt_by_comment(df):
+    return df.groupby('sentiment').count()['comment'].reset_index()
---
+++ app.py
@@ -0,0 +1,80 @@
+import streamlit as st
+import requests
+import Amazon as am
+import plotly.express as px
+st.title("Review Reveal")
+with st.sidebar:
+    image_url = ''
+    st.markdown(
+    f"""
+    <style>
+        .image-container {{
+            text-align: center;
+            padding-top: 0;  # Supprimer le padding en haut
+        }}
+    </style>
+    <div class= "image-container">
+        <img src="{image_url}"style="width:100px;">
+        <h1><u>Review Reveal</u></h1>
+    </div>
+    """,
+    unsafe_allow_html=True
+    )
+    st.header("a propose de projet")
+    description = """
+    Cette application utilise Streamlit pour analyser les avis des produits sur Amazon.
+    Elle fournit des analyses de sentiments et des informations sur les produits, 
+    permettant aux utilisateurs de prendre des décisions d'achat éclairées.
+    """
+    st.markdown(description)
+    github_link = """
+    [Consultez le code source sur GitHub](https://github.com/)
+    """
+    st.markdown(github_link)
+    linkedin_icon_url = "https://cdn-icons-png.flaticon.com/256/174/174857.png"
+    st.header("equipe")
+    equipe = """
+    - [El ouankrimi ali](https://www.linkedin.com/in/alielouankrimi/)
+    - [Olamine zakaria](www.linkedin.com/in/zakaria-olamine-20031115oz)
+    - [Oubella abdallah](https://www.linkedin.com/in/abdallah-oubella-2b5662239/)
+    """
+    st.markdown(equipe)
+url_produit = st.text_input("Entrez l'URL du produit Amazon:", placeholder="https://www.amazon.com/SAMSUNG-Android-Speakers-Upgraded-Graphite/dp/B0CLF2DNMV")
+if st.button("Analyser"):
+    with st.spinner("Analyse en cours ..."):
+        try:
+            reponse = requests.get(url_produit)
+            if reponse.status_code==200:
+                st.success("La verification de URL est avec success Analyse en cours, soyez patient...")
+                st.header("Informations générales sur le produit")
+                product_name = am.get_product_name(url_produit)
+                global_rating = am.get_global_rating(url_produit)
+                stars = am.get_rating(url_produit)
+                col1, col2 = st.columns([1, 1])
+                image_url = am.get_image_url(url_produit)
+                col1.image(image_url, caption='Image du produit', width=250)
+                col2.text(product_name)
+                col2.text(am.get_global_rating(url_produit))
+                col2.text(stars)
+                print(stars)
+                df = am.comment_dataFrame(url_produit)
+                print(df.shape[0])
+                xx = am.sentiemnt_by_comment(df)
+                x = px.bar(xx,x='sentiment',y='comment')
+                st.plotly_chart(x)
+                positive_comments = df[df['sentiment'] == 'Positive']['comment']
+                positive_text = ' '.join(positive_comments)
+                wordcloud = am.WordCloud(width=800, height=400, background_color='white').generate(positive_text)
+                fig = px.imshow(wordcloud.to_array(), binary_string=True)
+                fig.update_layout(title='Word Cloud for Positive Comments')
+                st.plotly_chart(fig)
+                negative_comments = df[df['sentiment'] == 'Negative']['comment']
+                negative_text = ' '.join(negative_comments)
+                wordcloud = am.WordCloud(width=800, height=400, background_color='white').generate(negative_text)
+                fig = px.imshow(wordcloud.to_array(), binary_string=True)
+                fig.update_layout(title='Word Cloud for Negative Comments')
+                st.plotly_chart(fig)
+            else:
+                st.error("La verification de Url est echouee Merci de verifier URL de votre produit")
+        except Exception as e:
+            st.error(f"Erreur pendant l'analyse : {str(e)}")
